4749eb3860a2625e23ff172dffe401245464f8d2
MuhdNihalCY <nihalcy1234@gmail.com>
2023-04-20 23:05:51 +0530

    started

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const slidingMenu = document.querySelector(".menu");
+    slidingMenu.classList.toggle("active");
+    const slidingClose = document.querySelector(".SmallScreen");
+    slidingClose.classList.toggle("active");
+  }
