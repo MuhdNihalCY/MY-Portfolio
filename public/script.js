860756a5d75b36910aa48393d0d4a9b494f2ee9c
MuhdNihalCY <nihalcy1234@gmail.com>
2023-05-12 00:02:40 +0530

    this commit will add loader for 3 seconds after site loadeed

--- public/script.js
+++ public/script.js
@@ -1,6 +1,23 @@
+// Hide the loader by default
+document.querySelector(".LoaderMain").style.display = "none";
+
+// Show the loader when the page is loaded
+window.addEventListener("load", function() {
+  document.querySelector(".LoaderMain").style.display = "flex";
+  document.querySelector(".Content").style.display = "none";
+});
+
+// Hide the loader after 3 seconds
+setTimeout(function() {
+  document.querySelector(".LoaderMain").style.display = "none";
+  document.querySelector(".Content").style.display = "block";
+}, 3000);
+
+
 function toggleMenu() {
     const slidingMenu = document.querySelector(".menu");
     slidingMenu.classList.toggle("active");
     const slidingClose = document.querySelector(".SmallScreen");
     slidingClose.classList.toggle("active");
   }
+
