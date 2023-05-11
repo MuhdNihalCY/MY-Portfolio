e853944453f44eebe53d8b33753c4632be155835
MuhdNihalCY <nihalcy1234@gmail.com>
2023-05-12 00:07:53 +0530

    remove the feature that loader only run only for first time

--- public/script.js
+++ public/script.js
@@ -3,22 +3,16 @@ document.querySelector(".LoaderMain").style.display = "none";
 
 // Show the loader when the page is loaded
 window.addEventListener("load", function() {
-  // Check if the loader has already been displayed
-  if (localStorage.getItem("loader-displayed") === null) {
-    // Display the loader
-    document.querySelector(".LoaderMain").style.display = "flex";
-    document.querySelector(".Content").style.display = "none";
-
-    // Hide the loader after 3 seconds
-    setTimeout(function() {
-      document.querySelector(".LoaderMain").style.display = "none";
-      document.querySelector(".Content").style.display = "block";
-    }, 3000);
-
-    // Store a value in localStorage to indicate that the loader has been displayed
-    localStorage.setItem("loader-displayed", "true");
-  }
+  document.querySelector(".LoaderMain").style.display = "flex";
+  document.querySelector(".Content").style.display = "none";
 });
+
+// Hide the loader after 3 seconds
+setTimeout(function() {
+  document.querySelector(".LoaderMain").style.display = "none";
+  document.querySelector(".Content").style.display = "block";
+}, 3000);
+
 
 function toggleMenu() {
     const slidingMenu = document.querySelector(".menu");
